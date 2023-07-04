32d04e816ca901955d9328c2753c818dba628f70
Isaque Akamine <isaque.akamine@cwi.com.br>
2023-07-03 23:36:23 -0300

    feat: creating welcome screen

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Title } from "./welcome-style";
+
+export default function WelcomeScreen() {
+  return (
+    <Container>
+      <Title>Welcome screen </Title>
+    </Container>
+  );
+}
