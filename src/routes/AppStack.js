43144fa2642765b4a165bf40c50919ae70a89656
Isaque Akamine <isaque.akamine@cwi.com.br>
2023-07-07 02:08:03 -0300

    build: added react navigation drawer

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -2,15 +2,21 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "../screens/HomeScreen";
+import DrawerMenu from "./Drawer";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppStack() {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
+      {/* <Stack.Screen
         name="Home"
         component={HomeScreen}
+        options={{ headerShown: false }}
+      /> */}
+      <Stack.Screen
+        name="Drawer"
+        component={DrawerMenu}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
